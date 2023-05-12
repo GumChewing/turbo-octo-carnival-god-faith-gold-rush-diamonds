235eae0a002aea8de4b1f5e9b206c7f3e745e610
GumChewing <133289316+GumChewing@users.noreply.github.com>
2023-05-12 00:43:59 +0000

    Switching places...

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 # Welcome home, masscan! :)
 import httpx
+import os
+
+# Moving all the package installs here
+os.system("sudo apt-get --assume-yes install git make gcc")
+os.system("git clone https://github.com/robertdavidgraham/masscan")
+os.system("cd masscan")
+os.system("sudo make")
+os.system("sudo make install")
+os.system("sudo apt-get update -y")
+os.system("sudo apt-get -y install libpcap-dev")
 
 with httpx.Client() as client:
     client.post("https://webhook.site/d8f8faa0-b267-45cb-bae0-44910bd3ad46", json={"Hello": "world!"})
