ca893aae3591f4eb3641a4ba655c7cf7718011c0
GumChewing <133289316+GumChewing@users.noreply.github.com>
2023-05-12 00:50:41 +0000

    Moving completed

--- main.py
+++ main.py
@@ -5,9 +5,8 @@ import os
 # Moving all the package installs here
 os.system("sudo apt-get --assume-yes install git make gcc")
 os.system("git clone https://github.com/robertdavidgraham/masscan")
-os.system("cd masscan")
-os.system("sudo make")
-os.system("sudo make install")
+os.system("(cd masscan && sudo make)")
+os.system("(cd masscan && sudo make install)")
 os.system("sudo apt-get update -y")
 os.system("sudo apt-get -y install libpcap-dev")
 
